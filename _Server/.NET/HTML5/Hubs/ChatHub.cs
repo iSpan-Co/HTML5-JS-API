f0ef30a79456dbe4213e0594b032c920a087d4e6
emma <hy0816@outlook.com>
2024-06-25 16:16:36 +0800

    feat: 調整練習題

--- _Server/.NET/HTML5/Hubs/ChatHub.cs
+++ _Server/.NET/HTML5/Hubs/ChatHub.cs
@@ -11,21 +11,6 @@ namespace HTML5.Hubs
         /// </summary>
         public override async Task OnConnectedAsync()
         {
-
-            //if (ConnIDList.Where(p => p == Context.ConnectionId).FirstOrDefault() == null)
-            //{
-            //    ConnIDList.Add(Context.ConnectionId);
-            //}
-            //// 更新連線 ID 列表
-            //string jsonString = JsonConvert.SerializeObject(ConnIDList);
-            //await Clients.All.SendAsync("UpdList", jsonString);
-
-            //// 更新個人 ID
-            //await Clients.Client(Context.ConnectionId).SendAsync("UpdSelfID", Context.ConnectionId);
-
-            //// 更新聊天內容
-            //await Clients.All.SendAsync("UpdContent", "新連線 ID: " + Context.ConnectionId);
-
             await Clients.All.SendAsync("UpdContent", "新連線 ID: " + Context.ConnectionId);
             await base.OnConnectedAsync();
         }
@@ -37,18 +22,6 @@ namespace HTML5.Hubs
         /// <returns></returns>
         public override async Task OnDisconnectedAsync(Exception ex)
         {
-            //string id = ConnIDList.Where(p => p == Context.ConnectionId).FirstOrDefault();
-            //if (id != null)
-            //{
-            //    ConnIDList.Remove(id);
-            //}
-            //// 更新連線 ID 列表
-            //string jsonString = JsonConvert.SerializeObject(ConnIDList);
-            //await Clients.All.SendAsync("UpdList", jsonString);
-
-            //// 更新聊天內容
-            //await Clients.All.SendAsync("UpdContent", "已離線 ID: " + Context.ConnectionId);
-
             await Clients.All.SendAsync("UpdContent", "已離線 ID: " + Context.ConnectionId);
             await base.OnDisconnectedAsync(ex);
         }
@@ -61,17 +34,6 @@ namespace HTML5.Hubs
         public async Task SendMessage(string user, string message)
         {
             await Clients.All.SendAsync("UpdContent", user + " 說: " + message);
-            //if (string.IsNullOrEmpty(user))
-            //{
-            //}
-            //else
-            //{
-            //    // 接收人
-            //    await Clients.Client(sendToID).SendAsync("UpdContent", selfID + " 私訊向你說: " + message);
-
-            //    // 發送人
-            //    await Clients.Client(Context.ConnectionId).SendAsync("UpdContent", "你向 " + sendToID + " 私訊說: " + message);
-            //}
         }
 
     }
